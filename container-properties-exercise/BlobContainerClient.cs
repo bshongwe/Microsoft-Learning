6821c3ba8321b32daf54125fd7b06764561f6632
bshongwe <dru.da.beat.hustle@gmail.com>
2023-07-15 00:56:03 +0200

    Set and retrieve properties and metadata for blob resources by using REST

--- container-properties-exercise/BlobContainerClient.cs
+++ container-properties-exercise/BlobContainerClient.cs
@@ -1,3 +1,4 @@
+// #1. Retrieve container properties
 private static async Task ReadContainerPropertiesAsync(BlobContainerClient container)
 {
     try
@@ -7,6 +8,53 @@ private static async Task ReadContainerPropertiesAsync(BlobContainerClient conta
         Console.WriteLine($"Properties for container {container.Uri}");
         Console.WriteLine($"Public access level: {properties.Value.PublicAccess}");
         Console.WriteLine($"Last modified time in UTC: {properties.Value.LastModified}");
+    }
+    catch (RequestFailedException e)
+    {
+        Console.WriteLine($"HTTP error code {e.Status}: {e.ErrorCode}");
+        Console.WriteLine(e.Message);
+        Console.ReadLine();
+    }
+}
+
+// #2. Set and retrieve metadata
+// 2.1 Set metadata
+public static async Task AddContainerMetadataAsync(BlobContainerClient container)
+{
+    try
+    {
+        IDictionary<string, string> metadata =
+           new Dictionary<string, string>();
+
+        // Add some metadata to the container.
+        metadata.Add("docType", "textDocuments");
+        metadata.Add("category", "guidance");
+
+        // Set the container's metadata.
+        await container.SetMetadataAsync(metadata);
+    }
+    catch (RequestFailedException e)
+    {
+        Console.WriteLine($"HTTP error code {e.Status}: {e.ErrorCode}");
+        Console.WriteLine(e.Message);
+        Console.ReadLine();
+    }
+}
+
+// 2.2 Retrieve metadata
+public static async Task ReadContainerMetadataAsync(BlobContainerClient container)
+{
+    try
+    {
+        var properties = await container.GetPropertiesAsync();
+
+        // Enumerate the container's metadata.
+        Console.WriteLine("Container metadata:");
+        foreach (var metadataItem in properties.Value.Metadata)
+        {
+            Console.WriteLine($"\tKey: {metadataItem.Key}");
+            Console.WriteLine($"\tValue: {metadataItem.Value}");
+        }
     }
     catch (RequestFailedException e)
     {
