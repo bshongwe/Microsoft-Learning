9d175ce6a0acd1b96519c472846faf9788aa4d80
bshongwe <dru.da.beat.hustle@gmail.com>
2023-07-16 22:46:19 +0200

    blob Program.cs update

--- blob-storage-exercise/Program.cs
+++ blob-storage-exercise/Program.cs
@@ -1,22 +1,53 @@
 using Azure.Storage.Blobs;
 using Azure.Storage.Blobs.Models;
+using System;
+using System.Threading.Tasks;
 
-Console.WriteLine("Azure Blob Storage exercise\n");
-
-// Run the examples asynchronously, wait for the results before proceeding
-ProcessAsync().GetAwaiter().GetResult();
-
-Console.WriteLine("Press enter to exit the sample application.");
-Console.ReadLine();
-
-static async Task ProcessAsync()
+class Program
 {
-    // Copy the connection string from the portal in the variable below.
-    string storageConnectionString = "CONNECTION STRING";
+    static async Task Main()
+    {
+        Console.WriteLine("Azure Blob Storage exercise\n");
 
-    // Create a client that can authenticate with a connection string
-    BlobServiceClient blobServiceClient = new BlobServiceClient(storageConnectionString);
+        // Run the examples asynchronously, wait for the results before proceeding
+        await ProcessAsync();
 
-    // COPY EXAMPLE CODE BELOW HERE
+        Console.WriteLine("Press enter to exit the sample application.");
+        Console.ReadLine();
+    }
 
+    static async Task ProcessAsync()
+    {
+        // Copy the connection string from the portal in the variable below.
+        string storageConnectionString = "CONNECTION STRING";
+
+        // Create a client that can authenticate with a connection string
+        BlobServiceClient blobServiceClient = new BlobServiceClient(storageConnectionString);
+
+        // Example code: Create a container and upload a blob
+        string containerName = "mycontainer";
+        string blobName = "myblob.txt";
+        string content = "Hello, Azure Blob Storage!";
+
+        BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(containerName);
+        await containerClient.CreateIfNotExistsAsync();
+
+        BlobClient blobClient = containerClient.GetBlobClient(blobName);
+        await blobClient.UploadTextAsync(content);
+
+        Console.WriteLine("Blob uploaded successfully!");
+
+        // Example code: Download the blob
+        BlobDownloadInfo download = await blobClient.DownloadAsync();
+        string downloadedContent = await download.Content.ReadAsStringAsync();
+
+        Console.WriteLine("Downloaded content:");
+        Console.WriteLine(downloadedContent);
+
+        // Example code: Delete the blob and container
+        await blobClient.DeleteAsync();
+        await containerClient.DeleteAsync();
+
+        Console.WriteLine("Blob and container deleted successfully!");
+    }
 }
